89de8e5e2cf0585188edb8fdc2d4b92533cd81d1
FrozenHeavenz <chenjingxuevernon@gmail.com>
2020-06-17 16:19:31 +0800

    Updated by Vernon Tan

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public static GameManager thisManager = null;
     private Animation thisAnimation;
+    public float Flapflap;
+    public Rigidbody rb;
+    public Text Score;
+    public float points;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = (transform.up * Flapflap);
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Death")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GG");
+        }
+        if(other.gameObject.tag == "BlockBlank")
+        {
+            points += 10;
+            Score.text = "SCORE : " + points;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GG");
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
